cd52759ce606d373e586180cfed4f9836f623890
Hossam Basuone <hossam.basuone@bosta.co>
2025-09-04 15:42:57 +0300

    fianl

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {  createHashRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Layout";
 import Map1 from "./Maps/Map1";
 
 import Map2 from "./Maps/Map2";
 import Map3 from "./Maps/Map3";
 import "./App.css";
-let router = createBrowserRouter([
+
+
+let router = createHashRouter([
   { path: "", element: <Layout /> },
   { path: "/map1", element: <Map1 /> },
   { path: "/map2", element: <Map2 /> },
